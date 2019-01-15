245fc39886ee3ea1177211de29e7638971fd73d0
Antonio Nunes <antonionunes@me.com>
2019-01-15 06:25:25 +0100

    Initial Commit

---
+++ PleasantNavigationController/PleasantNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  PleasantNavigationController.h
+//  PleasantNavigationController
+//
+//  Created by Antonio Nunes on 15/01/2019.
+//  Copyright © 2019 SintraWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PleasantNavigationController.
+FOUNDATION_EXPORT double PleasantNavigationControllerVersionNumber;
+
+//! Project version string for PleasantNavigationController.
+FOUNDATION_EXPORT const unsigned char PleasantNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PleasantNavigationController/PublicHeader.h>
+
+
